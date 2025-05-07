cd70513345ab45eed7217a0dfd0c20a819186b64
PARADISE-PARADOX <whoamiparadise@gmail.com>
2025-05-07 20:23:18 +0800

    Update markdown_regexp.ts

--- markdown_regexp.ts
+++ markdown_regexp.ts
@@ -5,8 +5,25 @@ export const rules:Rules = {
         
         heading:/^(#{1,6})\s+(.+)$/, //标题
         bold:/\*\*(.+?)\*\*/g, //加粗
-        italic:/\*(.+?)\*/ //斜体
+        italic:/\*(.+?)\*/, //斜体
 
-        link:/\[(.+?)\]\((.+?))\)/,
+        link:/\[(.+?)\]\((.+?))\)/, 
+        image:/!\[(.+?)\]\((.+?))\)/, 
+        inlineCode:/`(.+?)`/,
+        hr:/^([-*_]{3,0})$/,
+        list:/^(-|\+|\*|\d+\.)\s+(.+)$/,
+        blockquote:/^>\s+(.+)$/,
+        codeBlock:/^```(\w*)\n(.+?)\n```$/,
+        strikethrough:/^~~(.+?)~~$/,
+        underline:/^_(.+?)_$/,
+        subscript:/~(.+?)~/,
+        superscript:/^(.+?)^/,
+        audio:/!\[audio\]\((.+?)\)/,
+        checkboxUnchecked:/^\s*[-+*]\s*\[\s*\]\s+(.+)$/,
+        checkboxChecked:/^\s*[-+*]\s*\[\s*[xX]\s*\]\s+(.+)$/,
+        highlight:/==(.+?)==/,
+        math
+
+
     }
 }
